9effa770ab8b3514b1dba179b27d44af7c04d40e
jjwyse <joshua.wyse@gmail.com>
2013-09-16 19:13:09 -0600

    Playing around with GroovyUnitTest

--- test/src/com/jjw/sandbox/groovy/SamplePogoTest.groovy
+++ test/src/com/jjw/sandbox/groovy/SamplePogoTest.groovy
@@ -2,9 +2,9 @@ package com.jjw.sandbox.groovy;
 
 class SamplePogoTest extends GroovyTestCase
 {
-    void test()
+    void testSamplePogoEquals()
     {
-        SamplePogo sample = new SamplePogo(name:"Josh", address:"121 Main St");
+        SamplePogo sample = new SamplePogo();
         sample.setAddress("Address 1");
         sample.setName("Name1");
 
@@ -12,8 +12,10 @@ class SamplePogoTest extends GroovyTestCase
 
         System.out.println("Sample object: " + sample);
         assertFalse(sample.equals(sample2));
+        assertFalse(sample.hashCode().equals(sample2.hashCode()));
         sample2.setAddress("Address 1");
         sample2.setName("Name1");
         assertTrue(sample.equals(sample2));
+        assertTrue(sample.hashCode().equals(sample2.hashCode()));
     }
 }
