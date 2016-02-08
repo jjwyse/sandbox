96e194664c1bf9714e93dda9b7fd9a6ddd7f9977
jjwyse <josh@cloud-elements.com>
2016-02-08 16:02:51 -0700

    playing around w node libs

---
+++ src/main/java/com/jjw/sandbox/main/Main.java
@@ -0,0 +1,17 @@
+package com.jjw.sandbox.main;
+
+/**
+ * TODO - JJW
+ * @author jjwyse
+ */
+public class Main {
+    public static void main(String[] args) {
+        String stuart = "stuart";
+
+        if (stuart.equalsIgnoreCase("stuart")) {
+            System.out.println("foo");
+        } else if (stuart.equalsIgnoreCase("josh")) {
+            System.out.println("bar");
+        }
+    }
+}
