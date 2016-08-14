a80e1e6e6b9a1b8673ac70d25d1393e3ef8d4215
jjwyse <josh@cloud-elements.com>
2016-08-14 13:16:46 -0600

    readability

--- src/node/filter.js
+++ src/node/filter.js
@@ -1,13 +1,3 @@
-const results = [
-{
-   "id": 1,
-   "name": "josh"
-},
-{
-   "id": 2,
-   "name": "laura"
-}
-];
-
+const results = [ { "id": 1, "name": "josh" }, { "id": 2, "name": "laura" } ];
 const id = results.filter(i => i.name === 'josh')[0].id;
 console.log(id);
